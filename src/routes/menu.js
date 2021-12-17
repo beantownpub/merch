4ee4c000ce83f189e112fc5dab22721e1aa4503d
jonny graves <jonny.graves@hqo.co>
2021-12-17 15:38:37 -0500

    Cleanup

--- src/routes/menu.js
+++ src/routes/menu.js
@@ -1,47 +1,20 @@
 var express = require('express')
 var router = express.Router()
 const axios = require('axios')
+const getRequest = require('../utils/request')
 
-const API_USERNAME = process.env.API_USERNAME
-const API_PASSWORD = process.env.API_PASSWORD
-const AUTH = 'Basic ' + Buffer.from(API_USERNAME + ':' + API_PASSWORD).toString('base64')
-const HEADERS = {'Content-Type': 'application/json', 'Authorization': AUTH}
+var HEADERS = require('../utils/auth')
 
 function makeRequest(endpoint, res) {
   try {
     const host = process.env.MENU_API_HOST
     const protocol = process.env.MENU_API_PROTOCOL || 'https'
-    const api_url = `${protocol}://${host}/v1/menu/section/${endpoint}`
-    axios({
-      method: 'get',
-      url: api_url,
-      headers: HEADERS
-    })
-      .then(response => {
-        if (response.status === 200) {
-          res.status(200).json({
-            'status': 200,
-            'data': response.data
-          })
-        } else {
-          res.status(500).json({
-            'status': 500,
-            'message': 'Menu API Error'
-          })
-        }
-        res.end()
-      })
-      .catch(error => {
-        console.error('AXIOS Error: ' + error)
-        res.status(500).json({
-          'title': 'Menu Failure',
-          'status': 500
-        })
-      })
+    const apiUrl = `${protocol}://${host}/v1/menu/section/${endpoint}`
+    getRequest(apiUrl, res)
   } catch(error) {
-    console.log('AUTH Error: ' + error)
+    console.log('Request Error: ' + error)
     res.status(500).json({
-      'title': 'Menu Auth Failure',
+      'title': 'Request Failure',
       'status': 500
     })
   }
